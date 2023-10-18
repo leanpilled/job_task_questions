ddaad44a6a0941a91a88aa430fb9ac66f7b28b1d
leanpilled <112974379+leanpilled@users.noreply.github.com>
2023-10-18 19:59:39 +0300

    Update question_api.py

--- app/questions/question_api.py
+++ app/questions/question_api.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from fastapi import HTTPException, status
 import httpx
 
 from app.questions.dao import QuestionsDAO
@@ -8,6 +9,11 @@ async def fetch_data(num: int):
         response = await client.get(f'https://jservice.io/api/random?count={num}')
         if response.status_code == 200:
             return response.json()
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="invalid request"
+            )
 
 
 async def add_questions(num: int):
