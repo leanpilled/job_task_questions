657abd3dece8d0aa709f73687b2e71286c5c6f80
leanpilled <112974379+leanpilled@users.noreply.github.com>
2023-10-18 19:37:41 +0300

    Update router.py

--- app/questions/router.py
+++ app/questions/router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 
 from app.questions.dao import QuestionsDAO
 from app.questions.question_api import add_questions
-from app.questions.schemas import QuestionSchema
+from app.questions.schemas import QuestionRequest, QuestionSchema
 
 
 router = APIRouter(
@@ -12,7 +12,7 @@ router = APIRouter(
 )
 
 @router.post("")
-async def post_questions(questions: int) -> Optional[QuestionSchema]:
+async def post_questions(questions: QuestionRequest) -> Optional[QuestionSchema]:
     latest_question = await QuestionsDAO.get_latest_question()
-    await add_questions(questions)
+    await add_questions(questions.questions_num)
     return latest_question
